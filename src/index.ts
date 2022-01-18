81d0776e357f9ae899920aab689747437a1d1abd
harumaxy <harumaxy@gmail.com>
2022-01-19 01:32:57 +0900

    implement Wallet & Chain.addBlock meth

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import * as crypto from "crypto"; // Node built-in lib
 class Transaction {
   constructor(
     public amount: number,
-    public payer: string,
-    public payee: string
+    public payer: string, // publicKey
+    public payee: string // publicKey
   ) {}
 
   toString() {
@@ -42,11 +42,42 @@ class Chain {
   addBlock(
     transaction: Transaction,
     senderPublicKey: string,
-    signature: string
+    signature: Buffer
   ) {
-    const newBlock = new Block(this.lastBlock.hash, transaction);
-    this.chain.push(newBlock);
+    const verifier = crypto.createVerify("SHA256");
+    verifier.update(transaction.toString());
+
+    const isValid = verifier.verify(senderPublicKey, signature);
+
+    // 送り主の Public Key で検証. 正しければ Chain に Block (Transaction の履歴) を追加できる
+    if (isValid) {
+      const newBlock = new Block(this.lastBlock.hash, transaction);
+      this.chain.push(newBlock);
+    }
   }
 }
 
-class Wallet {}
+class Wallet {
+  public publicKey: string;
+  public privateKey: string;
+
+  constructor() {
+    const keyPair = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: "spki", format: "pem" },
+      privateKeyEncoding: { type: "pkcs8", format: "pem" },
+    });
+    this.publicKey = keyPair.publicKey;
+    this.privateKey = keyPair.privateKey;
+  }
+
+  sendMoney(amount: number, payeePublic: string) {
+    const transaction = new Transaction(amount, this.publicKey, payeePublic);
+
+    const sign = crypto.createSign("SHA256");
+    sign.update(transaction.toString()).end();
+
+    const signature = sign.sign(this.privateKey);
+    Chain.instance.addBlock(transaction, this.publicKey, signature);
+  }
+}
