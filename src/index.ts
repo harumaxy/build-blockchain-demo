88cc6d6bfa01fa855d0e62eb7c2fbeea8e1c8190
harumaxy <harumaxy@gmail.com>
2022-01-19 00:58:03 +0900

    implement Chain class (singleton)

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,26 @@ class Block {
   }
 }
 
-class Chain {}
+// singleton
+class Chain {
+  public static instance = new Chain();
+  chain: Block[];
+  constructor() {
+    this.chain = [new Block("", new Transaction(100, "genesis", "satoshi"))];
+  }
+
+  get lastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(
+    transaction: Transaction,
+    senderPublicKey: string,
+    signature: string
+  ) {
+    const newBlock = new Block(this.lastBlock.hash, transaction);
+    this.chain.push(newBlock);
+  }
+}
 
 class Wallet {}
